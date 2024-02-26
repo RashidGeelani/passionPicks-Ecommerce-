86e1fa08fc9fc9ee5f316a4f04028156a698da2b
rashid geelani <syed.geelani@vit.edu.in>
2024-02-26 19:02:38 +0530

    home Page

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,20 @@ import {
 } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import NoPage from "./pages/nopage/NoPage";
+import ProductInfo from "./pages/productInfo/ProductInfo";
+import ScrollTop from "./components/scrollTop/ScrollTop";
+import CartPage from "./pages/cart/CartPage";
 
 const App = () => {
   return (
     <div>
       <Router>
+        <ScrollTop />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/*" element={<NoPage />} />
+          <Route path="/productinfo" element={<ProductInfo />} />
+          <Route path="/cart" element={<CartPage />} />
         </Routes>
       </Router>
     </div>
