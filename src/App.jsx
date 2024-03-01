d898667a58fcec310dffebc3f7fdf3ff53b8bd47
rashid geelani <syed.geelani@vit.edu.in>
2024-03-01 13:41:53 +0530

    cp

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/*" element={<NoPage />} />
-          <Route path="/productinfo" element={<ProductInfo />} />
+          {/* <Route path="/productinfo" element={<ProductInfo />} /> */}
           <Route path="/cart" element={<CartPage />} />
         </Routes>
       </Router>
